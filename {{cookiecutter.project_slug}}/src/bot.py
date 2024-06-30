53350b85d43628447edb7835f10b90c8a241ed5d
Ihor Peresunko <marazmatic77@gmail.com>
2024-06-30 18:09:38 +0300

    Added base telegram bot logic

--- {{cookiecutter.project_slug}}/src/bot.py
+++ {{cookiecutter.project_slug}}/src/bot.py
@@ -1,5 +1,16 @@
+from loguru import logger
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
 from src import configs
 
 
-def main():
-    print('hello')
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+def main() -> None:
+    app = ApplicationBuilder().token(configs.BOT_TOKEN).build()
+    app.add_handler(CommandHandler('hello', hello))
+    logger.debug('Bot has started')
+    app.run_polling()
